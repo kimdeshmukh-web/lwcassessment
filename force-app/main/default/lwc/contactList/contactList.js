dabb47c300b6f7902041de11bf285404381af084
kimdeshmukh-web <deshmukhkim@gmail.com>
2023-11-05 21:27:44 +0530

    second commit

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,20 +1,25 @@
 import { LightningElement, wire, track } from 'lwc';
 import getContacts from '@salesforce/apex/demotclass.getContacts';
 
+
 export default class ContactList extends LightningElement {
-    @track contacts;
-    @track error;
+    columns = [
+        { label: 'Name',  fieldName: 'Name' },
+        { label: 'Email', fieldName: 'Email' },
+        { label: 'Phone', fieldName: 'Phone' }
+    ];
+    contacts;
+    rowOffset = 0;
 
     @wire(getContacts)
     wiredContacts({ error, data }) {
         if (data) {
             this.contacts = data;
-            this.error = null;
         } else if (error) {
-            this.contacts = null;
-            this.error = 'Error fetching Contacts';
+            console.error(error);
         }
     }
-
-    
+    increaseRowOffset() {
+        this.rowOffset += 100;
+    }
 }
